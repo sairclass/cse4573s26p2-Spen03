4ea302a8957681d08dee83894604c9b6e607800c
Spen03 <jensenspencer00@gmail.com>
2026-03-24 18:49:41 -0400

    +Commented out the bonus parts of the bash script because the bonus images kill my computer. Perhaps they would run on computers with NVIDIA GPUs using cuda hardware acceleration, which I have implemented in my code.

--- pack_submission.sh
+++ pack_submission.sh
@@ -6,8 +6,10 @@ if find "images/Bonus1" -type f -iname '*.png' -print -quit | grep -q .; then
     python task2.py --input_path images/Bonus1 --output outputs/bonus1.png --json ./bonus1.json
 fi
 
+'''
 if find "images/Bonus2" -type f -iname '*.png' -print -quit | grep -q .; then
     python task2.py --input_path images/Bonus2 --output outputs/bonus2.png --json ./bonus2.json
 fi
+'''
 
 python utils.py --ubit $1
